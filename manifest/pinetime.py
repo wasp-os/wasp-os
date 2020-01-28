6dfc95764f40b6546b81b120ba1fca0b498db77c
Daniel Thompson <daniel@redfelineninja.org.uk>
2020-01-28 21:16:22 +0000

    manifest: Enable highest optimization by default

--- manifest/pinetime.py
+++ manifest/pinetime.py
@@ -1,8 +1,10 @@
-freeze_as_mpy('$(MPY_DIR)/../wasp', (
-    'main.py',
-    'demo.py',
-    'drivers/st7789.py',
-    'logo.py',
-    'pinetime.py'
-))
-
+freeze('$(MPY_DIR)/../wasp',
+    (
+        'main.py',
+        'demo.py',
+        'drivers/st7789.py',
+        'logo.py',
+        'pinetime.py'
+    ),
+    opt=3
+)
