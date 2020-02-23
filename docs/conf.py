3ba356f6c4a1b823444dd45138f46976ab252f66
Daniel Thompson <daniel@redfelineninja.org.uk>
2020-02-23 20:16:23 +0000

    docs: Add a sphinx board
    The simulator board does rather too much when we import watch.py. This
    change means we don't need SDL just to generate the documentation!

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../wasp'))
-sys.path.insert(0, os.path.abspath('../wasp/boards/simulator'))
+sys.path.insert(0, os.path.abspath('../wasp/boards/sphinx'))
 
 
 # -- Project information -----------------------------------------------------
