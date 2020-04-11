4c09add2d8df214ebacbefb4e7890b3dc58b2c06
Daniel Thompson <daniel@redfelineninja.org.uk>
2020-04-11 20:49:06 +0100

    docs: Add text descriptions of icons
    Having the raw hex strings makes the docs look really unclean. Fix this
    by providing a special version of icons for the "sphinx" board.

---
+++ wasp/boards/sphinx/icons.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2020 Daniel Thompson
+
+battery = 'Default battery icon'
+bomb = 'Small bomb icon'
+app = 'Default application icon'
+clock = 'Default digital clock icon'
+settings = 'Default settings icon'
+torch = 'Default torch or flashlight icon'
+up_arrow = 'Small (16x9) up arrow'
+down_arrow = 'Small (16x9) down arrow'
