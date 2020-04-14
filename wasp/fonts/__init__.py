a77301ec64b9c3f88e699e89c554b21aa18fc50b
Daniel Thompson <daniel@redfelineninja.org.uk>
2020-04-14 20:00:04 +0100

    fonts: Add sans36 (numerals only)
    At 36pt this is already gobbling a fair bit of flash so, for now, we've
    restricted this only numerals (and colon).

---
+++ wasp/fonts/__init__.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2020 Daniel Thompson
+
+import fonts.sans24 as sans24
+import fonts.sans36 as sans36
+
+def height(font):
+    return font.height()
+
+def width(font, s):
+    w = 0
+    for ch in s:
+        (_, _, wc) = font.get_ch(ch)
+        w += wc + 1
+
+    return w
+
+
