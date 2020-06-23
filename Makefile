wasp-os/wasp-os

commit 64afca127612d6d9ded4ac86a7e76fbe3d8086f1

Makefile: Fix make sim (allow BOARD to be unset)
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

Makefile
@@ -2,7 +2,13 @@ export PYTHONPATH := $(PWD)/tools/nrfutil:$(PWD)/tools/intelhex:$(PYTHONPATH)
 
 all : bootloader reloader micropython
 
+ifdef BOARD
+WASP_WATCH_PY = wasp/boards/$(BOARD)/watch.py
+$(WASP_WATCH_PY) : $(WASP_WATCH_PY).in
+	(cd wasp; ../tools/preprocess.py $(WASP_WATCH_PY).in $(WASP_WATCH_PY))
+else
 BOARD ?= $(error Please set BOARD=)
+endif
 
 clean :
 	$(RM) -r \
@@ -31,7 +37,7 @@ reloader: bootloader
 softdevice:
 	micropython/ports/nrf/drivers/bluetooth/download_ble_stack.sh
 
-micropython: wasp/boards/$(BOARD)/watch.py
+micropython: $(WASP_WATCH_PY)
 	$(MAKE) -C micropython/mpy-cross
 	$(RM) micropython/ports/nrf/build-$(BOARD)-s132/frozen_content.c
 	$(MAKE) -C micropython/ports/nrf \
@@ -43,10 +49,6 @@ micropython: wasp/boards/$(BOARD)/watch.py
 		--dev-type 0x0052 \
 		--application micropython/ports/nrf/build-$(BOARD)-s132/firmware.hex \
 		micropython.zip
-
-wasp/boards/$(BOARD)/watch.py : wasp/boards/$(BOARD)/watch.py.in
-	(cd wasp; ../tools/preprocess.py boards/$(BOARD)/watch.py.in > \
-		                         boards/$(BOARD)/watch.py)
 
 dfu:
 	python3 -m nordicsemi dfu serial --package micropython.zip --port /dev/ttyACM0

commit adcc67cd6115efef07291bd071c55542bca9ef54

Makefile: Fix builds when watch.py must be regenerated
The paths passed to the preprocessor are currently wrong. Fix them!

Fixes: 64afca1 ("Makefile: fix make sim (allow BOARD to be unset)")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

Makefile
@@ -5,7 +5,7 @@ all : bootloader reloader micropython
 ifdef BOARD
 WASP_WATCH_PY = wasp/boards/$(BOARD)/watch.py
 $(WASP_WATCH_PY) : $(WASP_WATCH_PY).in
-	(cd wasp; ../tools/preprocess.py $(WASP_WATCH_PY).in $(WASP_WATCH_PY))
+	(cd wasp; ../tools/preprocess.py ../$(WASP_WATCH_PY).in > ../$(WASP_WATCH_PY))
 else
 BOARD ?= $(error Please set BOARD=)
 endif
@@ -15,7 +15,8 @@ clean :
 		bootloader/_build-$(BOARD)_nrf52832 \
 		reloader/build-$(BOARD) reloader/src/boards/$(BOARD)/bootloader.h \
 		micropython/mpy-cross/build \
-		micropython/ports/nrf/build-$(BOARD)-s132
+		micropython/ports/nrf/build-$(BOARD)-s132 \
+		$(WASP_WATCH_PY)
 
 submodules :
 	git submodule update --init --recursive