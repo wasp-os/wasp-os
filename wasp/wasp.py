wasp-os/wasp-os

commit 889115f4b081428ebfcf0f71c7ab405fb22c2527

wasp: Allow app initialization to fail
Colmi has released a new revision of the P8 hardware based on a different
accelerometer. That makes it impossible for the StepCounterApp to
initialize and currently this takes down the whole GUI due to the
uncaught exception.

Fix this by skipping applications that will not initialize.

Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/wasp.py
@@ -115,13 +115,19 @@ class Manager():
         self._scheduling = False
 
         # TODO: Eventually these should move to main.py
-        self.register(ClockApp(), True)
-        self.register(StepCounterApp(), True)
-        self.register(StopwatchApp(), True)
-        self.register(HeartApp(), True)
-        self.register(FlashlightApp(), False)
-        self.register(SettingsApp(), False)
-        self.register(TestApp(), False)
+        for app, qr in ( (ClockApp, True),
+			 (StepCounterApp, True),
+			 (StopwatchApp, True),
+			 (HeartApp, True),
+			 (FlashlightApp, False),
+			 (SettingsApp, False),
+			 (TestApp, False) ):
+		try:
+			self.register(app(), qr)
+		except:
+			# Let's not bring the whole device down just because there's
+			# an exception starting one of the apps...
+			pass
 
     def register(self, app, quick_ring=False):
         """Register an application with the system.

commit 7075f5689eacbf586324495b1fda3cc1baf03173

wasp: Fix broken indention
Currently the simulator cannot run correctly because the indentation
in Manager.__init__ mixes tab and spaces. This is something CPython3
doesn't like but MicroPython is ok about.

Fix the obvious by using correct indentation.

Fixes: 889115f ("wasp: Allow app initialization to fail")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/wasp.py
@@ -116,18 +116,18 @@ class Manager():
 
         # TODO: Eventually these should move to main.py
         for app, qr in ( (ClockApp, True),
-			 (StepCounterApp, True),
-			 (StopwatchApp, True),
-			 (HeartApp, True),
-			 (FlashlightApp, False),
-			 (SettingsApp, False),
-			 (TestApp, False) ):
-		try:
-			self.register(app(), qr)
-		except:
-			# Let's not bring the whole device down just because there's
-			# an exception starting one of the apps...
-			pass
+                         (StepCounterApp, True),
+                         (StopwatchApp, True),
+                         (HeartApp, True),
+                         (FlashlightApp, False),
+                         (SettingsApp, False),
+                         (TestApp, False) ):
+            try:
+                self.register(app(), qr)
+            except:
+                # Let's not bring the whole device down just because there's
+                # an exception starting one of the apps...
+                pass
 
     def register(self, app, quick_ring=False):
         """Register an application with the system.