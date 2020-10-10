c1c66fb487113acbd4b2b7a722c3782d5a962e08
Daniel Thompson <daniel@redfelineninja.org.uk>
2020-10-10 14:04:44 +0100

    boards: Introduce a shared manifest for all 240x240 devices
    This also adds the Fibonaci Clock, Haiku viewer and the Game of Life
    to the manifest. They are *not* registered by default at this point
    since, although we can currently spare the internal flash space there
    is more competition for RAM so we have to trade off out-of-the-box
    convenience with keeping as much RAM as possible for users to do
    "cool things".
    
    Given the zen of wasp-os is to try to make is as easy as possible for
    users to become coders we currently favour reserving the space for the
    cool things (and implicitly encouraging them to write a couple of lines
    of python to enable the bonus applications.
    
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

---
+++ wasp/boards/manifest_240x240.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2020 Daniel Thompson
+"""Shared manifest for applications that work well on a 240x240 display."""
+
+manifest = (
+    'apps/clock.py',
+    'apps/flashlight.py',
+    'apps/gameoflife.py',
+    'apps/haiku.py',
+    'apps/heart.py',
+    'apps/launcher.py',
+    'apps/pager.py',
+    'apps/settings.py',
+    'apps/steps.py',
+    'apps/stopwatch.py',
+    'apps/fibonacci_clock.py',
+    'apps/testapp.py',
+    'fonts/__init__.py',
+    'fonts/clock.py',
+    'fonts/sans24.py',
+    'fonts/sans28.py',
+    'fonts/sans36.py',
+    'icons.py',
+    'widgets.py',
+)
