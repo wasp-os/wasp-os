8b108f7be2d8cbf2fc7641c469e7f2437c293776
kozova1 <mug66kk@gmail.com>
2020-11-17 12:04:18 +0200

    Added basic functionality, need to add win checking
    Signed-off-by: kozova1 <mug66kk@gmail.com>

--- wasp/apps/connect4.py
+++ wasp/apps/connect4.py
@@ -10,16 +10,21 @@
 
 import wasp
 import array
+import draw565
+
 
 class Connect4App():
     """Application implementing the classic Connect 4 game."""
-    NAME = 'Connect 4'
+    NAME = 'Connect4'
     ROWS = 6
     COLUMNS = 7
+    MODES = ('Playing', 'Win', 'Draw')
+
     def __init__(self):
         """Initialize the app"""
         self._board = array.array('B', [0] * (6*7))
-        self._color = 1 # 1 = red, 2 = yellow
+        self._color = 1 # 1 = red(0xF800), 2 = yellow(0xFFC0)
+        self._mode = 'Playing'
     
     def foreground(self):
         """Activate the app"""
@@ -30,18 +35,47 @@ class Connect4App():
         """Draw the display"""
         draw = wasp.watch.drawable
         draw.fill()
-        for i in range(1, 7):
-            draw.fill(i * (240//COLUMNS), 0, 1, 240) # fill vertical lines
+        if self._mode == 'Playing':
+            for i in range(1, 7):
+                draw.fill(None, i * (240//self.COLUMNS), 0, 1, 240) # fill vertical lines
 
-        for i in range(1, 6):
-            draw.fill(0, i * (240//ROWS), 240, 1)    # fill horizontal lines
+            for i in range(1, 6):
+                draw.fill(None, 0, i * (240//self.ROWS), 240, 1)    # fill horizontal lines
 
-        self._update()
+            self._update()
+
 
     def _update(self):
         """Update dynamic parts of display (win message & discs)"""
-        pass
+        for y in range(6):
+            for x in range(7):
+                i = x + y * 7
+                if self._board[i] == 0:
+                    continue
+                dx = (x * 240) // 7
+                dy = (y * 240) // 6
+                color = 0xF800 if self._board[i] == 1 else 0xFFC0
+                wasp.watch.drawable.fill(color, dx, dy, 240//self.COLUMNS, 240//self.ROWS)
 
     def touch(self, event):
         """Notify the app of a touchscreen touch"""
-        pass
+        if self._mode == 'Playing':
+            column = (self.COLUMNS * event[1]) // 240
+            disks_in_column = 0
+            first_free_place = None
+            # Count the amount of disks in the column we want.
+            # If it's full, we'll ignore the event
+            for i in range(35, -1, -7):
+                if self._board[i + column] != 0:
+                    disks_in_column += 1
+                else:
+                    first_free_place = (i + column) if first_free_place is None else first_free_place
+
+            if disks_in_column >= 6:
+                return # column is full, can't place piece here!
+
+            self._board[first_free_place] = self._color
+            self._color = 3 - self._color # yellow(2) => red(1), red(1) => yellow(2)
+            self._update()
+
+        
