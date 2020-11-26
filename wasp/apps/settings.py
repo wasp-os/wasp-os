wasp-os/wasp-os

commit b1326e16092babc7d353f2fdc724b6decc63c129

apps: settings: Fix initial slider value
When the Settings app is launched, it shows the "Mid" text (this is
correct), but the slider is at the lowest position. After moving the
slider it functions correctly, so this fix is mostly cosmetic.

Signed-off-by: kozova1 <mug66kk@gmail.com>

wasp/apps/settings.py
@@ -19,6 +19,7 @@ class SettingsApp():
 
     def __init__(self):
         self._slider = wasp.widgets.Slider(3, 10, 90)
+        self._slider.value = wasp.system.brightness - 1
 
     def foreground(self):
         self._draw()

commit 4c0120aa1ea93a280824e96e9221b8dbee82121f

apps: settings: Avoid circular dependancies during app init
wasp.system cannot be safely be used from app __init__ methods. Move
the initial value settings to the foreground method instead.

Fixes: b1326e16092b ("apps: settings: Fix initial slider value")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/apps/settings.py
@@ -19,9 +19,9 @@ class SettingsApp():
 
     def __init__(self):
         self._slider = wasp.widgets.Slider(3, 10, 90)
-        self._slider.value = wasp.system.brightness - 1
 
     def foreground(self):
+        self._slider.value = wasp.system.brightness - 1
         self._draw()
         wasp.system.request_event(wasp.EventMask.TOUCH)
 