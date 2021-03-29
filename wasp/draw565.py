wasp-os/wasp-os

commit cc34c5d46de99adf049c1e164d873f1ed0f3cd28

draw565: Fix wrong background color of strings Signed-off-by: Joris Warmbier <siroj42@t-online.de>

wasp/draw565.py
@@ -291,21 +291,23 @@ class Draw565(object):
         display = self._display
         bgfg = self._bgfg
         font = self._font
+        bg = self._bgfg >> 16
 
         if width:
             (w, h) = _bounding_box(s, font)
             leftpad = (width - w) // 2
             rightpad = width - w - leftpad
-            self.fill(0, x, y, leftpad, h)
+            self.fill(bg, x, y, leftpad, h)
             x += leftpad
 
         for ch in s:
             glyph = font.get_ch(ch)
             _draw_glyph(display, glyph, x, y, bgfg)
+            self.fill(bg, x+glyph[2], y, 1, glyph[1])
             x += glyph[2] + 1
 
         if width:
-            self.fill(0, x, y, rightpad, h)
+            self.fill(bg, x, y, rightpad, h)
 
     def wrap(self, s, width):
         """Chunk a string so it can rendered within a specified width.

commit 58b5c0378ec3d5421a4af2bfed0434e3a286a1b0

draw565: Optimize the string drawing
Currently there is a redundant fill operation issued for every character
drawn. This was added to draw the background colours correctly but the
change did not account for the optimized character rendering in
_draw_glyph().

This results in ~15% performance improvement for character rendering

Fixes: cc34c5d46de9 ("draw565: Fix wrong background color of strings")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/draw565.py
@@ -87,8 +87,8 @@ def _draw_glyph(display, glyph, x, y, bgfg):
     (px, h, w) = glyph
 
     buf = display.linebuffer[0:2*(w+1)]
-    buf[2*w] = 0
-    buf[2*w + 1] = 0
+    buf[2*w] = bgfg >> 24
+    buf[2*w + 1] = (bgfg >> 16) & 0xff
     bytes_per_row = (w + 7) // 8
 
     display.set_window(x, y, w+1, h)
@@ -324,7 +324,6 @@ class Draw565(object):
         for ch in s:
             glyph = font.get_ch(ch)
             _draw_glyph(display, glyph, x, y, bgfg)
-            self.fill(bg, x+glyph[2], y, 1, glyph[1])
             x += glyph[2] + 1
 
         if width: