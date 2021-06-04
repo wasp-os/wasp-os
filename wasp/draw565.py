wasp-os/wasp-os

commit 58b5c0378ec3d5421a4af2bfed0434e3a286a1b0

draw565: Optimize the string drawing
Currently there is a redundant fill operation issued for every character
drawn. This was added to draw the background colours correctly but the
change did not account for the optimized character rendering in
_draw_glyph().

This results in ~15% performance improvement for character rendering

Fixes: cc34c5d46de9 ("draw565: Fix wrong background color of strings")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/draw565.py
@@ -87,8 +87,8 @@ def _draw_glyph(display, glyph, x, y, bgfg):
     (px, h, w) = glyph
 
     buf = display.linebuffer[0:2*(w+1)]
-    buf[2*w] = 0
-    buf[2*w + 1] = 0
+    buf[2*w] = bgfg >> 24
+    buf[2*w + 1] = (bgfg >> 16) & 0xff
     bytes_per_row = (w + 7) // 8
 
     display.set_window(x, y, w+1, h)
@@ -324,7 +324,6 @@ class Draw565(object):
         for ch in s:
             glyph = font.get_ch(ch)
             _draw_glyph(display, glyph, x, y, bgfg)
-            self.fill(bg, x+glyph[2], y, 1, glyph[1])
             x += glyph[2] + 1
 
         if width:

commit 4ad827390227249ba69f1ae60671b20531fdd9e1

draw565: Fix bounding box calculations
wasp-os uses an drawing optimization technique to automatically place
a single pixel line on the right of glyphs when rendering them. This
results in a change to the bounding box for a rendered string (by adding
a single pixel on the right of the final character). Fix the bounding box
calculations accordingly. Among other things this eliminates graphical
artifacts when rendering labels in 2048.

Fixes: #203
Fixes: 58b5c0378ec3 ("draw565: Optimize the string drawing")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/draw565.py
@@ -75,7 +75,7 @@ def _bounding_box(s, font):
         return (0, font.height())
 
     get_ch = font.get_ch
-    w = len(s) - 1
+    w = len(s)
     for ch in s:
         (_, h, wc) = get_ch(ch)
         w += wc

wasp/boards/simulator/test_unit.py
@@ -53,7 +53,7 @@ def test_font_width(draw):
     for f in (fonts.sans24, fonts.sans28, fonts.sans36):
         draw.set_font(f)
 
-        assert 3 == draw.bounding_box('0000')[0] % 4
+        assert 0 == draw.bounding_box('0000')[0] % 4
 
         if f.max_ch() >= 90:
             assert draw.bounding_box('IIII')[0] < draw.bounding_box('WWWW')[0]