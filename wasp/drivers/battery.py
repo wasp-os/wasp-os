wasp-os/wasp-os

commit fb18705b9b9cc10e15e56cea6697839f3779bdb6

manager/rtc: Experimental power saving technique
Currently the time is calculated 8 times per second from (relatively) slow
python code. Optimize the power consumed by reducing the number of times
we check for wall time updates to only once-per-second and use native
code generation to reduce VM overhead when executing this critical code.

At the time of writing the difference is battery life has not yet been
measured (but we know the current master branch is worse than v0.4 and,
in theory at least, this should close the gap).

Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/drivers/nrf_rtc.py
@@ -45,6 +45,7 @@ class RTC(object):
             self._uptime = 0
             self.set_localtime((2020, 3, 1, 3, 0, 0, 0, 0))
 
+    @micropython.native
     def update(self):
         """Check for counter updates.
 
@@ -59,10 +60,12 @@ class RTC(object):
 
         self.lastcount += split
         self.lastcount &= (1 << 24) - 1   
+        uptime = self._uptime
         self._uptime += split
         machine.mem32[0x200039c8] = self._uptime * 125
 
-        return True
+        # Has the seconds count changed
+        return bool((self._uptime ^ uptime) & 0x08)
 
     def set_localtime(self, t):
         """Set the current wall time.
@@ -103,6 +106,7 @@ class RTC(object):
         localtime = self.get_localtime()
         return localtime[3:6]
 
+    @micropython.native
     def time(self):
         """Get time in the same format as time.time"""
         return self.offset + (self._uptime >> 3)

wasp/wasp.py
@@ -430,6 +430,7 @@ class Manager():
 
         watch.touch.reset_touch_data()
 
+    @micropython.native
     def _tick(self):
         """Handle the system tick.
 

commit f221e2f8a4e52f0d14e7d59ceddf65785195129a

rtc: Undo the once-per-second wake up
So... waking up once per second turns out to be a dumb idea because it
regresses the stop watch and HRS tools (which now also only wake up once
per second).

Undo this change but sprinkle a few more micropython.native decorations
on methods used on the wakeup path to minimise power.

Fixes: fb18705b9b9cc ("manager/rtc: Experimental power saving technique")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

wasp/drivers/battery.py
@@ -4,6 +4,7 @@
 """Generic lithium ion battery driver
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
+import micropython
 from machine import Pin, ADC
 
 class Battery(object):
@@ -26,6 +27,7 @@ class Battery(object):
         self._charging = charging
         self._power = power
 
+    @micropython.native
     def charging(self):
         """Get the charging state of the battery.
 

wasp/drivers/nrf_rtc.py
@@ -62,11 +62,11 @@ class RTC(object):
         self.lastcount += split
         self.lastcount &= (1 << 24) - 1   
         uptime = self._uptime
-        self._uptime += split
-        machine.mem32[0x200039c8] = self._uptime * 125
+        uptime += split
+        machine.mem32[0x200039c8] = uptime * 125
+        self._uptime = uptime
 
-        # Has the seconds count changed
-        return bool((self._uptime ^ uptime) & 0x08)
+        return True
 
     def set_localtime(self, t):
         """Set the current wall time.
@@ -117,6 +117,7 @@ class RTC(object):
         """Provide the current uptime in seconds."""
         return self._uptime // 8
 
+    @micropython.native
     def get_uptime_ms(self):
         """Return the current uptime in milliseconds."""
         return self._uptime * 125

wasp/drivers/touch.py
@@ -6,6 +6,7 @@
 """
 
 import array
+import micropython
 import time
 from machine import Pin
 from watch import rtc
@@ -45,6 +46,7 @@ class TouchButton:
         if self.schedule:
             self.schedule(self)
 
+    @micropython.native
     def get_event(self):
         """Receive a touch event.
 