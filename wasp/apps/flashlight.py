a3b2e24d404b5ac50f059f72d90e27c4bc763517
k <k@klabz.org>
2021-09-20 22:18:19 +0200

    Flashlight: add on/off (start off)
    Signed-off-by: k <k@klabz.org>

--- wasp/apps/flashlight.py
+++ wasp/apps/flashlight.py
@@ -19,13 +19,15 @@ class TorchApp(object):
     NAME = 'Torch'
     ICON = icons.torch
 
+    def __init__(self):
+        self.__activated = False
+
     def foreground(self):
         """Activate the application."""
+        self._brightness = wasp.system.brightness
         self.draw()
         wasp.system.request_tick(1000)
-
-        self._brightness = wasp.system.brightness
-        wasp.system.brightness = 3
+        wasp.system.request_event(wasp.EventMask.TOUCH | wasp.EventMask.BUTTON)
 
     def background(self):
         """De-activate the application (without losing state)."""
@@ -34,6 +36,38 @@ class TorchApp(object):
     def tick(self, ticks):
         wasp.system.keep_awake()
 
+    def touch(self, event):
+        self.__activated = not self.__activated
+        self.draw()
+
+    def press(self, button, state):
+        if not state:
+            return
+
+        self.__activated = not self.__activated
+        self.draw()
+
     def draw(self):
         """Redraw the display from scratch."""
-        wasp.watch.drawable.fill(0xffff)
+        white = 0xffff
+        draw = wasp.watch.drawable
+
+        if self.__activated:
+            draw.fill(white)
+            wasp.system.brightness = 3
+
+        else:
+            draw.fill()
+            wasp.system.brightness = self._brightness
+
+            x = 108
+            mid = wasp.system.theme('mid')
+
+            draw.fill(mid, x, 107, 24, 9)
+            for i in range(1, 8):
+                draw.line(x+i, 115+i, x+23-i, 115+i, color=mid)
+            draw.fill(mid, x+8, 123, 8, 15)
+
+            draw.line(x-3, 94, x+5, 102, 2, white)
+            draw.line(x+17, 102, x+25, 94, 2, white)
+            draw.line(x+11, 89, x+11, 100, 2, white)
