b5f4099fa6f2eb43de847aed673d0d9a29ef726f
Francesco Gazzetta <fgaz@fgaz.me>
2021-10-03 19:21:52 +0200

    Test and build docs for user defined apps too
    Signed-off-by: Francesco Gazzetta <fgaz@fgaz.me>

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../wasp'))
+sys.path.insert(0, os.path.abspath('../apps'))
 sys.path.insert(0, os.path.abspath('../wasp/boards/sphinx'))
 
 
