b680d813169102cf94697655ca88f7a1ca32a47d
tigercoding56 <90169211+tigercoding56@users.noreply.github.com>
2023-02-18 22:13:06 -0800

    move some stuff around

--- wasp/boards/manifest_240x240.py
+++ wasp/boards/manifest_240x240.py
@@ -28,7 +28,7 @@ manifest = (
     'apps/testapp.py',
     'apps/timer.py',
     'apps/weather.py',
-    'apps/draw.py',
+    'apps/flappybird.py',
     'apps/week_clock.py',
     'apps/word_clock.py',
     'fonts/__init__.py',
