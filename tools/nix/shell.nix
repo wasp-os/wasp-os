0424efd304e56e46feb1be8c838d137e568dddfb
Francesco Gazzetta <fgaz@fgaz.me>
2023-03-29 18:13:03 +0200

    Add nix development shell
    Signed-off-by: Francesco Gazzetta <fgaz@fgaz.me>

---
+++ tools/nix/shell.nix
@@ -0,0 +1,36 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # wasptool and ota-dfu only work on linux, and their dependencies prevent the
+  # shell to evaluate on darwin
+  ifLinux = pkgs.lib.optionals pkgs.stdenv.isLinux;
+
+in pkgs.mkShell {
+  nativeBuildInputs = [
+    (pkgs.python3.withPackages (pp: with pp; [
+      cbor
+      click
+      cryptography
+      dbus-python
+      numpy
+      pexpect
+      pillow
+      pygobject3
+      pysdl2
+      pyserial
+      tomli
+
+      pytest
+
+      # Docs
+      recommonmark
+      sphinx
+    ] ++ ifLinux [
+      bluepy
+    ]))
+    pkgs.gcc-arm-embedded
+    pkgs.graphviz
+  ] ++ ifLinux [
+    pkgs.bluez
+  ];
+}
